c83a4616600d969e15b33892ed98fddb9e0bba83
sproute <prestiz4321@gmail.com>
2022-03-19 17:30:24 +0300

    first commit

---
+++ lib/plugins/desktop/io.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+void setTargetPlatformForDesktop({TargetPlatform? platform}) {
+  TargetPlatform? targetPlatform;
+  if (platform != null) {
+    targetPlatform = platform;
+  }
+  if (targetPlatform == null) {
+    if (Platform.isMacOS) {
+      targetPlatform = TargetPlatform.iOS;
+    } else if (Platform.isLinux || Platform.isWindows) {
+      targetPlatform = TargetPlatform.android;
+    } else if (Platform.isWindows) {
+      targetPlatform = TargetPlatform.windows;
+    }
+  }
+  debugDefaultTargetPlatformOverride = targetPlatform;
+}
