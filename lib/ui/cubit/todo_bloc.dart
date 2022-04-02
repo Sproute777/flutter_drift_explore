dd55c0a4516a3de6599ac3f81c5592273518432c
sproute <prestiz4321@gmail.com>
2022-04-02 13:46:17 +0300

    clean

--- lib/ui/cubit/todo_bloc.dart
+++ lib/ui/cubit/todo_bloc.dart
@@ -9,17 +9,17 @@ import '../../my_drift_database/todos_dao.dart';
 part 'todo_state.dart';
 
 class TodoCubit extends Cubit<TodoState> {
-  final TodosDao _database;
+  final TodosDao _repository;
   StreamSubscription? _subscriptionTodo;
   StreamSubscription? _subscriptionCategory;
 
-  TodoCubit(TodosDao database)
-      : _database = database,
+  TodoCubit(TodosDao repository)
+      : _repository = repository,
         super(TodoState()) {
-    _subscriptionTodo = _database.homeScreenEntries?.listen((event) {
+    _subscriptionTodo = _repository.homeScreenEntries?.listen((event) {
       _fresh(event);
     });
-    _subscriptionCategory = _database.ctg.listen((event) {
+    _subscriptionCategory = _repository.ctg.listen((event) {
       _newCatefories(event);
     });
   }
@@ -30,12 +30,12 @@ class TodoCubit extends Cubit<TodoState> {
   void _newCatefories(List<CategoryWithActiveInfo> category) =>
       emit(state.newCategories(c: category));
 
-  void createTodo(String text) => _database.createTodo(text);
-  void deleteEntry(TodoEntry entry) => _database.deleteTodo(entry);
-  void updateEntry(TodoEntry entry) => _database.updateTodo(entry);
+  void createTodo(String text) => _repository.createTodo(text);
+  void deleteEntry(TodoEntry entry) => _repository.deleteTodo(entry);
+  void updateEntry(TodoEntry entry) => _repository.updateTodo(entry);
 
-  void addCategory(String text) => _database.createCategory(text);
-  // void deleteCategory(Category category) => _database.deleteCategory(category);
+  void addCategory(String text) => _repository.createCategory(text);
+  // void deleteCategory(Category category) => _repository.deleteCategory(category);
 
   Future<void> close() {
     _subscriptionTodo?.cancel();
